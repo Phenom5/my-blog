a0aa6a838d649dfb4216e75156988befc1596f93
Emmanuel <eokoro252@gmail.com>
2022-03-10 15:39:02 +0100

    third commit

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blogs</title>
+</head>
+<body>
+    <article>
+        <h1>First blog post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolor omnis aliquid.
+         Cumque error assumenda, eos fugiat expedita incidunt, sit iusto perspiciatis aut hic quam, 
+         saepe placeat natus commodi porro.</p>
+    </article>
+
+    <article>
+        <h1>Second blog post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolor omnis aliquid.
+         Cumque error assumenda, eos fugiat expedita incidunt, sit iusto perspiciatis aut hic quam, 
+         saepe placeat natus commodi porro.</p>
+    </article>
+
+    <article>
+        <h1>Third blog post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolor omnis aliquid.
+         Cumque error assumenda, eos fugiat expedita incidunt, sit iusto perspiciatis aut hic quam, 
+         saepe placeat natus commodi porro.</p>
+    </article>
+
+    <article>
+        <h1>Fourth blog post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolor omnis aliquid.
+         Cumque error assumenda, eos fugiat expedita incidunt, sit iusto perspiciatis aut hic quam, 
+         saepe placeat natus commodi porro.</p>
+    </article>
+
+    <article>
+        <h1>Fifth blog post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolor omnis aliquid.
+         Cumque error assumenda, eos fugiat expedita incidunt, sit iusto perspiciatis aut hic quam, 
+         saepe placeat natus commodi porro.</p>
+    </article>
+
+</body>
+</html>
